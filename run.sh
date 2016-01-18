68c7a3f89f7d631e455c20ef096769af2508aeb4
Benjamin Schmid <benjamin.schmid@hetras.com>
2016-01-18 17:45:27 +0100

    When run.sh gets SIGINT, SIGTERM or SIGKILL it should also stop all subprocesses

--- run.sh
+++ run.sh
@@ -4,9 +4,21 @@ aws_creds=`grep ANSIBLE_AWS_KEY_FILE /etc/environment`
 export ${token}
 export ${aws_creds}
 
+# kill subprocess
+function kill_subprocess {
+  local PID=${1}
+  for child in $(ps -o pid --no-headers --ppid ${PID}); do
+    kill_subprocess ${child}
+  done
+  kill -9 ${PID} > /dev/null 2>&1
+}
+
 # run hubot controlled by supervisord
 cd hubot
-bin/hubot -a slack
+trap 'echo "Killing $PID"; kill_subprocess $PID' TERM INT KILL
+bin/hubot -a slack &
+PID=$!
+wait $PID
 
-# docker try by crodemeyer was never fully functional - comment out by Benni
+# docker try by crodemeyer was never fully functional - commented out by Benni
 #docker run --rm -it -e "AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID" -e "AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY" hubot
