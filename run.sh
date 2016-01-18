6ed16f89b316290d3d3c34b981182e102e7deab2
Benjamin Schmid <benjamin.schmid@hetras.com>
2016-01-18 11:41:24 +0100

    Adapted run script to make heinz a supervisord task

--- run.sh
+++ run.sh
@@ -1,1 +1,10 @@
-docker run --rm -it -e "AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID" -e "AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY" hubot 
+#! /bin/bash
+token=`grep SLACK /etc/environment`
+export ${token}
+
+# run hubot controlled by supervisord
+cd hubot
+bin/hubot -a slack
+
+# docker try by crodemeyer was never fully functional - comment out by Benni
+#docker run --rm -it -e "AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID" -e "AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY" hubot
